4ed303bf022079ad81058f46fbac154999240e50
Yash Mishra <mishrayash392@gmail.com>
2025-08-20 01:20:31 +0530

    added ws server for exchanging chat messages

--- wsBackend/src/index.ts
+++ wsBackend/src/index.ts
@@ -1,9 +1,36 @@
-import {WebSocketServer} from "ws" 
+import   { WebSocketServer,WebSocket } from "ws"; 
 
+interface allSocket {
+    socket :WebSocket  , 
+    roomid:string 
+}
+
+const allSockets :allSocket[] = []
 const wss = new WebSocketServer({port:8080}) 
 
-wss.on('connection',(socket)=>{
-    socket.on('message',(e)=>{
 
+wss.on("connection",(socket)=>{
+    socket.on("message",(message)=>{ 
+        //@ts-ignore
+      const parsedMessage = JSON.parse(message ) ; 
+      
+      if(parsedMessage.type==="join"){ 
+       
+        allSockets.push({
+            socket , 
+            roomid:parsedMessage.payload.roomid
+        })
+      }
+
+      if(parsedMessage.type==="chat"){
+        const currentUser = allSockets.find((x)=>x.socket==socket)?.roomid; 
+        for(let i =0 ; i<allSockets.length;i++){
+            if(allSockets[i]?.roomid==currentUser){
+                allSockets[i]?.socket.send(parsedMessage.payload.message); 
+            }
+        }
+      }
     })
+
+
 })
