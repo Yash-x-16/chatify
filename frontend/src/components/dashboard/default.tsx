5718b4f93e147df92d8aee65b9477eaac35c5b5c
Yash_x_16 <mishrayash392@gmail.com>
2025-10-20 08:12:14 +0530

    rebuilding

--- frontend/src/components/dashboard/default.tsx
+++
@@ -1,9 +0,0 @@
-import { FaArrowLeft } from "react-icons/fa";
-export function DefaultCard(){
-    return <div className="w-full bg-inherit h-full rounded-2xl flex justify-center items-center gap-3">
-       <FaArrowLeft size={"22px"} className="text-white/50 mt-1"/>
-        <h1 className="text-white/50 text-2xl">
-            Select something from the navbar to see 
-        </h1>
-    </div>
-}
--- frontend/src/pages/homepage.tsx
+++
@@ -1,8 +0,0 @@
-import { HomepageComponent } from "../components/dashboard/homeComponent";
-import { DashBoardContext } from "../contexts/dashboardContext";
-
-export function Homepage(){
-    return <DashBoardContext>
-        <HomepageComponent/>
-    </DashBoardContext>
-}
